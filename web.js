ee6f19151ce4adb7436a3a97acf9e3678d256993
Gurjot Singh <g7dhaliwal@gmail.com>
2013-09-13 03:21:25 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -25,7 +25,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
  // var out = fs.buffer;
   //out.toString('utf-8');
-  response.send(data);
+  response.write(data);
 });
 
 var port = process.env.PORT || 5000;
